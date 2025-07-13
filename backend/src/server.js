2d2ca7ce6a47306fd2985bdd3800e9ebabf486cb
Hassam-Ata <hassamata86@gmail.com>
2025-07-14 02:04:58 +0500

    feat: implement database connection and user authentication routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,24 @@
 import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+
+import { connectDB } from "./config/db.js";
 
 const app = express();
+app.use(cors());
+app.use(clerkMiddleware());
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+connectDB();
 
 app.listen(5000, () => {
-  console.log("Server starte");
+  console.log("Server started");
 });
